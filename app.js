48e17dadd9c765f5cf5becb5671a2f1b94419274
Reason Adhikari <72090971+adhikariReason@users.noreply.github.com>
2022-11-03 12:05:22 -0500

    added icecream view and rendered harcoded data

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var icecreamRouter = require('./routes/icecream.js');
 
-
 var app = express();
 
 // view engine setup
@@ -24,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/icecream', icecreamRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
