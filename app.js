e1cc71f5cd09658c063b0d2e22e5baef786facbc
Reason Adhikari <72090971+adhikariReason@users.noreply.github.com>
2022-11-02 22:19:59 -0500

    added icecream class and provided routes to render in web server

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var icecreamRouter = require('./routes/icecream.js');
+
 
 var app = express();
 
@@ -21,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/icecream', icecreamRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/icecream.js
+++ routes/icecream.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Reason Adhikari' });
+  res.render('icecream', { title: 'Search Result' });
 });
 
 module.exports = router;
